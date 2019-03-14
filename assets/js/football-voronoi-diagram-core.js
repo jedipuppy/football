046a3616637b0bc4bb386c18885d240502e17d6c
jedipuppy <puppy@kaduo.jp>
2019-03-14 19:34:35 +0900

    速度のスライダーに目盛り追加、Save機能、１：１も動くように

--- assets/js/football-voronoi-diagram-core.js
+++ assets/js/football-voronoi-diagram-core.js
@@ -1,15 +1,18 @@
 // 設定値
-const AREAWIDTH = 400;
-const AREAHEIGHT = 800;
-const GOALWIDTH = 100;
-const GOALHEIGHT = 20;
+const AREAWIDTH = 800;
+const AREAHEIGHT = 400;
+const GOALWIDTH = 20;
+const GOALHEIGHT = 100;
 const COLOR_RED = '#F45D52';
 const COLOR_BLUE = '#465B73';
 const COLOR_AQUA = '#49A1DE';
 const COLOR_GREEN = '#f5f5dc';
 const COLOR_BLACK = '#696969';
+const COLOR_WHITE = '#FFFFFF';
 const PLAYERRADIUS = 10;
 const BALLRADIUS = 10;
+const SPEED_DELTA = 0.001;
+const TERRITORY_OPACITY = 0.3;
 let ball_speed = 6;
 let player_speed = 1;
 let players = [];
@@ -18,16 +21,27 @@ let coverlines = [];
 let ball;
 let snap_obj;
 let side = 1;
+let player_add_feature = 0;
 window.onload = function () {
   init();
 
 };
 
 
-
-
 function init() {
+  let speedslider = new rSlider({
+    target: '#speed-slider',
+    values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    range: false,
+    set: [5],
+    tooltip: false,
+    onChange: function (vals) {
+      console.log(vals);
+      RefreshTerritory(vals);
+    }
+  });
 
+  $('.ui.checkbox').checkbox();
 
   GenerateBackground();
 
@@ -35,27 +49,35 @@ function init() {
   let canvas = document.querySelector('.canvas');
   snap_obj.prependTo(canvas);
   //ballを配置
-  GenerateBall(AREAWIDTH / 2, 20);
+  GenerateBall(20, AREAHEIGHT / 2);
   side = -1;
-  GeneratePlayer(AREAWIDTH / 3, AREAHEIGHT / 2);
-  GeneratePlayer(2 * AREAWIDTH / 3, AREAHEIGHT / 2);
+  GeneratePlayer(AREAWIDTH / 2, AREAHEIGHT / 3, 1);
+  GeneratePlayer(AREAWIDTH / 2, 2 * AREAHEIGHT / 3, 1);
   side = 1;
-  GeneratePlayer(AREAWIDTH / 3, AREAHEIGHT / 3);
-  GeneratePlayer(2 * AREAWIDTH / 3, AREAHEIGHT / 3);
+  GeneratePlayer(AREAWIDTH / 3, AREAHEIGHT / 3, 1);
+  GeneratePlayer(AREAWIDTH / 3, 2 * AREAHEIGHT / 3, 1);
   //clickするとプレイヤーを追加する
   snap_obj.click(function (e) {
     if (Snap.getElementByPoint(e.clientX, e.clientY).objtype !== 'player' && Snap.getElementByPoint(e.clientX, e.clientY).objtype !== 'ball') {
-      GeneratePlayer(e.clientX, e.clientY);
+      GeneratePlayer(e.clientX, e.clientY, 0);
     }
   });
   //ダブルクリックするとプレイヤーを削除する
   snap_obj.dblclick(function (e) {
-    RemovePlayer(e.clientX, e.clientY);
+    RemovePlayer(e.clientX, e.clientY, 0);
   });
 }
 
-function ChangeSide() {
-  side *= -1;
+function ChangeSide(flag) {
+  if (flag === 0) {
+    player_add_feature = !player_add_feature;
+  } else if (flag === 1) {
+    player_add_feature = 1;
+    side = 1;
+  } else {
+    player_add_feature = 1;
+    side = -1;
+  }
 }
 
 function GenerateBall(x, y) {
@@ -71,30 +93,30 @@ function GenerateBall(x, y) {
   console.log('generate ball');
 }
 
-function GeneratePlayer(x, y) {
+function GeneratePlayer(x, y, flag) {
+  if (player_add_feature || flag) {
+    id = players.length;
+    console.log(x, y);
+    players.push(snap_obj.circle(x, y, PLAYERRADIUS));
+    players[id].side = side;
+    players[id].x = x;
+    players[id].y = y;
+    players[id].id = id;
+    players[id].objtype = 'player';
+    GenerateTerritory(ball.x, ball.y, x, y, id, side, 1);
 
-  id = players.length;
-  console.log(x, y);
-  players.push(snap_obj.circle(x, y, PLAYERRADIUS));
-  players[id].side = side;
-  players[id].x = x;
-  players[id].y = y;
-  players[id].id = id;
-  players[id].objtype = 'player';
-  GenerateTerritory(ball.x, ball.y, x, y, id, side, 1);
 
-
-  if (side === 1) {
-    players[id].attr({
-      fill: COLOR_RED
-    });
-  } else {
-    players[id].attr({
-      fill: COLOR_BLUE
-    });
+    if (side === 1) {
+      players[id].attr({
+        fill: COLOR_RED
+      });
+    } else {
+      players[id].attr({
+        fill: COLOR_BLUE
+      });
+    }
+    players[id].drag(DragMove, DragStart, DragStop);
   }
-  players[id].drag(DragMove, DragStart, DragStop);
-
 
 }
 
@@ -110,17 +132,17 @@ function GenerateTerritory(ball_x, ball_y, player_x, player_y, id, side, fresh_f
   if (side === 1) {
     territorys[id].attr({
       fill: COLOR_RED,
-      fillOpacity: 0.5
+      fillOpacity: TERRITORY_OPACITY
     });
   } else {
     territorys[id].attr({
       fill: COLOR_BLUE,
-      fillOpacity: 0.5
+      fillOpacity: TERRITORY_OPACITY
     });
   }
-  players[id].before(territorys[id]);
+  players[0].before(territorys[id]);
   if (players[id].side === 1) {
-    CoverLine(AREAWIDTH / 2, AREAHEIGHT, apollonius_x, apollonius_y, apollonius_r, id, fresh_flag, side);
+    CoverLine(AREAWIDTH, AREAHEIGHT / 2, apollonius_x, apollonius_y, apollonius_r, id, fresh_flag, side);
   } else {
     CoverLine(ball_x, ball_y, apollonius_x, apollonius_y, apollonius_r, id, fresh_flag, side);
   }
@@ -146,11 +168,11 @@ function CoverLine(ball_x, ball_y, apollonius_x, apollonius_y, apollonius_r, id,
     let va2 = b2 * d2;
     let vb2 = -a2 * d2;
     if (side == 1) {
-      line_temp = snap_obj.line(a + apollonius_x, b + apollonius_y, AREAWIDTH / 2, AREAHEIGHT).attr({
+      line_temp = snap_obj.line(a + apollonius_x, b + apollonius_y, AREAWIDTH, AREAHEIGHT / 2).attr({
         stroke: COLOR_RED,
         strokeWidth: 2,
       });
-      line_temp2 = snap_obj.line(a2 + apollonius_x, b2 + apollonius_y, AREAWIDTH / 2, AREAHEIGHT).attr({
+      line_temp2 = snap_obj.line(a2 + apollonius_x, b2 + apollonius_y, AREAWIDTH, AREAHEIGHT / 2).attr({
         stroke: COLOR_RED,
         strokeWidth: 2
       });
@@ -173,7 +195,6 @@ function CoverLine(ball_x, ball_y, apollonius_x, apollonius_y, apollonius_r, id,
       coverlines[id].push(line_temp);
       coverlines[id].push(line_temp2);
     } else {
-      console.log(coverlines, id);
       coverlines[id][0].remove();
       coverlines[id][1].remove();
       coverlines.splice(id, 1);
@@ -181,8 +202,11 @@ function CoverLine(ball_x, ball_y, apollonius_x, apollonius_y, apollonius_r, id,
       coverlines[id].push(line_temp);
       coverlines[id].push(line_temp2);
     }
-
+  } else {
+    coverlines[id][0].remove();
+    coverlines[id][1].remove();
   }
+
 }
 
 function CalcApollonius(ball_x, ball_y, player_x, player_y) {
@@ -196,39 +220,37 @@ function CalcApollonius(ball_x, ball_y, player_x, player_y) {
 function GenerateBackground() {
   // Snapオブジェクトを作成しサイズも指定
   snap_obj = Snap(AREAWIDTH, AREAHEIGHT);
-  background = snap_obj.rect(0, 0, AREAWIDTH, AREAHEIGHT).attr({
-    fill: COLOR_GREEN,
-    stroke: COLOR_BLACK,
-    strokeWidth: 2,
-  });
+
   field_line = []
-  field_line[0] = snap_obj.line(0, AREAHEIGHT / 2, AREAWIDTH, AREAHEIGHT / 2).attr({
+  field_line[0] = snap_obj.line(AREAWIDTH / 2, 0, AREAWIDTH / 2, AREAHEIGHT).attr({
     fill: COLOR_GREEN,
     stroke: COLOR_BLACK,
     strokeWidth: 2,
   });;
-  background = snap_obj.rect(AREAWIDTH / 2 - GOALWIDTH / 2, AREAHEIGHT - GOALHEIGHT, GOALWIDTH, GOALHEIGHT).attr({
+  background = snap_obj.rect(AREAWIDTH - GOALWIDTH, AREAHEIGHT / 2 - GOALHEIGHT / 2, GOALWIDTH, GOALHEIGHT).attr({
     fill: COLOR_BLACK,
     stroke: COLOR_BLACK,
     strokeWidth: 2,
-    fillOpacity: 0.5
+    fillOpacity: TERRITORY_OPACITY
   });
 }
 
-function RemovePlayer(x, y) {
-  remove_player = Snap.getElementByPoint(x, y);
-  console.log(remove_player)
+function RemovePlayer(x, y, flag) {
+  if (player_add_feature || flag) {
 
-  coverlines[remove_player.id][0].remove();
-  coverlines[remove_player.id][1].remove();
-  coverlines.splice(remove_player.id, 1);
+    remove_player = Snap.getElementByPoint(x, y);
+    console.log(remove_player)
 
-  remove_player.remove();
+    coverlines[remove_player.id][0].remove();
+    coverlines[remove_player.id][1].remove();
+    coverlines.splice(remove_player.id, 1);
 
-  players.splice(remove_player.id, 1);
-  territorys[remove_player.id].remove();
-  territorys.splice(remove_player.id, 1);
+    remove_player.remove();
 
+    players.splice(remove_player.id, 1);
+    territorys[remove_player.id].remove();
+    territorys.splice(remove_player.id, 1);
+  }
 
 }
 
@@ -265,12 +287,29 @@ function DragMove(dx, dy, x, y) {
 
 
 function RefreshTerritory(speed) {
-  ball_speed = speed;
+  ball_speed = parseFloat(speed) + SPEED_DELTA;
+
   for (i = 0; i < territorys.length; i++) {
-    console.log(ball_speed);
+    console.log("start", i, territorys.length);
     side_temp = territorys[i].side;
     territorys[i].remove();
     territorys.splice(i, 1);
     GenerateTerritory(ball.x, ball.y, players[i].x, players[i].y, i, side_temp, 0);
+  }
+}
+
+function Save() {
+
+  let blob = new Blob([snap_obj.outerSVG()], {
+    'type': 'image/svg+xml'
+  });
+  if (window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(blob, 'football-area.svg');
+
+    // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+    window.navigator.msSaveOrOpenBlob(blob, 'football-area.svg');
+  } else {
+    console.log(snap_obj.outerSVG());
+    document.getElementById('save').href = window.URL.createObjectURL(blob);
   }
 }
